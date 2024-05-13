ce9da7d193c410634b95616ec5772087da57af30
tomeipau <149239277+tomeipau@users.noreply.github.com>
2024-05-13 22:37:37 +0800

    Update ml_model_deploy.py

--- ml_model_deploy.py
+++ ml_model_deploy.py
@@ -54,5 +54,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-st.markdown("# WQD7006 Machine Learning For Data Science - Group 15")
-st.write("Law Jia Jin, Lim Sze Sing, Gan Jing Wen, Nur Shafiqah, Nur Nazifa")
+st.write("# WQD7006 Machine Learning For Data Science - Group 15 : Law Jia Jin, Lim Sze Sing, Gan Jing Wen, Nur Shafiqah, Nur Nazifa")
